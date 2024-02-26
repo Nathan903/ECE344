387dda7e02df837902e1b0baec1acd6bd9a5c445
nathan <noreply@nsa.gov>
2024-02-26 14:10:56 -0500

    asst3 start

---
+++ submit.sh
@@ -0,0 +1,9 @@
+git remote set-url origin ssh://ug250.eecg.toronto.edu/srv/ece344s/os-042/ece344
+git tag asst3-end
+git push --tags
+git remote set-url origin git@github.com:Nathan903/ECE344.git
+git push --force
+git push --tags --force
+cd ../marker3
+os161-tester -m  3
+
