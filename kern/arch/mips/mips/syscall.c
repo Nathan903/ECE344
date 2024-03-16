d33da5206b7bee80ab75fdc1a997163bb078ee16
nathan <noreply@nsa.gov>
2024-03-16 00:22:40 -0400

    fixed bug

--- kern/arch/mips/mips/syscall.c
+++ kern/arch/mips/mips/syscall.c
@@ -1,11 +1,11 @@
-#include <kern/callno.h>
+#include <types.h>
 #include <kern/errno.h>
 #include <lib.h>
 #include <machine/pcb.h>
 #include <machine/spl.h>
 #include <machine/trapframe.h>
+#include <kern/callno.h>
 #include <syscall.h>
-#include <types.h>
 
 /*
  * System call handler.
