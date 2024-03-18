7cdbb6e3926ce9c24550f088fb7f4a523f7cf606
nathan <noreply@nsa.gov>
2024-03-18 19:28:59 -0400

    change submit script

--- submit.sh
+++ submit.sh
@@ -1,9 +1,11 @@
 git remote set-url origin ssh://ug250.eecg.toronto.edu/srv/ece344s/os-060/ece344
-git tag asst3-end
+git tag asst4-end
+git push --tags
+git remote set-url origin ssh://ug250.eecg.toronto.edu/srv/ece344s/os-042/ece344
+git tag asst4-end
 git push --tags
 git remote set-url origin git@github.com:Nathan903/ECE344.git
 git push --force
 git push --tags --force
-cd ../marker3
-os161-tester -m  3
-
+cd ../marker4
+os161-tester -m  4
