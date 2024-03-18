0efd2fe6836716704797eade8527f2099f2477d9
nathan <noreply@nsa.gov>
2024-03-18 16:01:57 -0400

    done runprogram_with_args

--- kern/userprog/runprogram.c
+++ kern/userprog/runprogram.c
@@ -13,6 +13,7 @@
 #include <curthread.h>
 #include <vm.h>
 #include <vfs.h>
+#include <lib.h>
 #include <test.h>
 
 /*
@@ -73,12 +74,20 @@ runprogram(char *progname)
 	panic("md_usermode returned\n");
 	return EINVAL;
 }
+#define MAX_STR_LENGTH 100
+#define NOT_VALID_STR 69
 
+int strnlen(const char *s){
+	int ret = 0;
+	while (s[ret]) { 
+	ret++;
+	  if(ret>MAX_STR_LENGTH){ 
+	    return -1;
+	  }
+	}
+	return ret;
+}
 int runprogram_with_args(char *progname,char ** args, unsigned long nargs){
-  unsigned long i;for(i=0; i<nargs;i++){
-  kprintf(" %s ", args[i]);
-  } kprintf("\n");
-
   struct vnode *v;
   vaddr_t entrypoint, stackptr;
   int result;
@@ -119,10 +128,60 @@ int runprogram_with_args(char *progname,char ** args, unsigned long nargs){
     /* thread_exit destroys curthread->t_vmspace */
     return result;
   }
+  
+  
+    int args_copy_len = 4*(nargs+1);
+  vaddr_t *lengths = (vaddr_t*)kmalloc((nargs + 1) * sizeof(vaddr_t));
+  lengths[nargs] = (vaddr_t) 0;
 
+  int curlen = 0; int padding =0;
+
+  //kprintf("\n args_copy_len %d \n",args_copy_len);
+  unsigned long i;for(i=0; i<nargs;i++){
+    curlen =  strnlen(args[i]);
+    if (curlen ==-1){ return NOT_VALID_STR;}
+    padding = (4-(curlen+1)%4)%4;
+    assert( (curlen+padding+1)%4 == 0);
+    args_copy_len+= curlen+padding+1;
+    lengths[i] = (vaddr_t) (curlen+padding+1);
+  } assert( args_copy_len%4 == 0);
+  
+  stackptr -= (vaddr_t) args_copy_len;
+  //kprintf("\n args_copy_len %d \n",args_copy_len);
+  char *args_copy = (char *)kmalloc( args_copy_len );
+  int ii; for (ii = 0; ii < args_copy_len; ii++) { args_copy[ii] = '\0';}
+  unsigned int  idx=4*(nargs+1);
+  for(i=0; i<nargs;i++){
+    //kprintf("%u %d\n",idx,args_copy_len);
+    strcpy( (char *) (args_copy+idx), args[i]);
+    //kprintf("%u %d\n",idx,args_copy_len);
+    int final_loc = stackptr+idx;
+    idx += lengths[i];
+    lengths[i]= final_loc;
+  } 
+  /*
+  for (ii = 0; ii < args_copy_len; ii++) { 
+    if(args_copy[ii])
+    kprintf("%c", args_copy[ii]);
+    else kprintf("_");
+  } kprintf("\n");
+ for (ii = 0; ii < (int)nargs; ii++) { 
+    kprintf("%x ",lengths[ii]);
+  } kprintf("\n");
+  kprintf("%x\n", stackptr);
+  */
+  
+  copyout( (const void *)(args_copy), (userptr_t)stackptr, (size_t) args_copy_len);
+  copyout( (const void *)(lengths), (userptr_t)stackptr, (size_t) ((nargs + 1) * sizeof(vaddr_t)));
+  //kprintf("%x\n", stackptr);
+  
+  
   /* Warp to user mode. */
-  md_usermode(0 /*argc*/, NULL /*userspace addr of argv*/, stackptr,
-              entrypoint);
+  md_usermode(
+    (int) nargs, //argc
+    (userptr_t) stackptr /*userspace addr of argv*/,
+    stackptr,
+    entrypoint);
 
   /* md_usermode does not return */
   panic("md_usermode returned\n");
