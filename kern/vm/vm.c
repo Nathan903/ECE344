c71139b986b7de4ab13e4758831133de7b689220
Nathan <30803215+Nathan903@users.noreply.github.com>
2024-04-06 22:16:29 -0400

    Update vm.c

--- kern/vm/vm.c
+++ kern/vm/vm.c
@@ -130,12 +130,11 @@ int vm_fault(int faulttype, vaddr_t faultaddress) {
   return EFAULT;
 }
 
-struct addrspace *as_create(void) {
-  struct addrspace *as = kmalloc(sizeof(struct addrspace));
-  if (as == NULL) {
-    return NULL;
-  }
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct addrspace* as_create(void) {
+  struct addrspace* as = kmalloc(sizeof(struct addrspace));
+  if (as == NULL) { return NULL; }
+  //////////////////////START//////////////////////////
   as->as_vbase1 = 0;
   as->as_pbase1 = 0;
   as->as_npages1 = 0;
@@ -143,28 +142,78 @@ struct addrspace *as_create(void) {
   as->as_pbase2 = 0;
   as->as_npages2 = 0;
   as->as_stackpbase = 0;
+  //////////////////////END//////////////////////////
 
   return as;
 }
+int as_copy(struct addrspace *old, struct addrspace **ret) {
+  struct addrspace * new = as_create();
+  if (new == NULL) { return ENOMEM; }
+  //////////////////////START//////////////////////////
 
-void as_destroy(struct addrspace *as) { kfree(as); }
+  new->as_vbase1 = old->as_vbase1;
+  new->as_npages1 = old->as_npages1;
+  new->as_vbase2 = old->as_vbase2;
+  new->as_npages2 = old->as_npages2;
+
+  if (as_prepare_load(new)) {
+    as_destroy(new);
+    return ENOMEM;
+  }
+
+  assert(new->as_pbase1 != 0);
+  assert(new->as_pbase2 != 0);
+  assert(new->as_stackpbase != 0);
+
+  memmove((void *)PADDR_TO_KVADDR(new->as_pbase1),
+          (const void *)PADDR_TO_KVADDR(old->as_pbase1),
+          old->as_npages1 * PAGE_SIZE);
+
+  memmove((void *)PADDR_TO_KVADDR(new->as_pbase2),
+          (const void *)PADDR_TO_KVADDR(old->as_pbase2),
+          old->as_npages2 * PAGE_SIZE);
+
+  memmove((void *)PADDR_TO_KVADDR(new->as_stackpbase),
+          (const void *)PADDR_TO_KVADDR(old->as_stackpbase),
+          DUMBVM_STACKPAGES * PAGE_SIZE);
+  //////////////////////END//////////////////////////
+  *ret = new; return 0;
+}
+
+void as_destroy(struct addrspace *as) { 
+  //////////////////////START//////////////////////////
+  //Clean up as needed
+  //////////////////////END//////////////////////////
+  kfree(as); 
+}
 
 void as_activate(struct addrspace *as) {
-  int i, spl;
-
   (void)as;
-
+  //////////////////////START//////////////////////////
+  int i, spl;
   spl = splhigh();
 
   for (i = 0; i < NUM_TLB; i++) {
     TLB_Write(TLBHI_INVALID(i), TLBLO_INVALID(), i);
   }
-
   splx(spl);
+  //////////////////////END//////////////////////////
+
 }
 
-int as_define_region(struct addrspace *as, vaddr_t vaddr, size_t sz,
-                     int readable, int writeable, int executable) {
+/*
+ * Set up a segment at virtual address VADDR of size MEMSIZE. The
+ * segment in memory extends from VADDR up to (but not including)
+ * VADDR+MEMSIZE.
+ *
+ * The READABLE, WRITEABLE, and EXECUTABLE flags are set if read,
+ * write, or execute permission should be set on the segment. At the
+ * moment, these are ignored. When you write the VM system, you may
+ * want to implement them.
+ */
+int as_define_region(struct addrspace *as, vaddr_t vaddr, size_t sz, int readable, int writeable, int executable) {
+  //////////////////////START//////////////////////////
+
   size_t npages;
 
   /* Align the region. First, the base... */
@@ -197,10 +246,14 @@ int as_define_region(struct addrspace *as, vaddr_t vaddr, size_t sz,
    * Support for more than two regions is not available.
    */
   kprintf("dumbvm: Warning: too many regions\n");
+  //////////////////////END//////////////////////////
+
   return EUNIMP;
 }
 
 int as_prepare_load(struct addrspace *as) {
+  //////////////////////START//////////////////////////
+
   assert(as->as_pbase1 == 0);
   assert(as->as_pbase2 == 0);
   assert(as->as_stackpbase == 0);
@@ -219,56 +272,22 @@ int as_prepare_load(struct addrspace *as) {
   if (as->as_stackpbase == 0) {
     return ENOMEM;
   }
+  //////////////////////END//////////////////////////
 
   return 0;
 }
 
 int as_complete_load(struct addrspace *as) {
+  //////////////////////START//////////////////////////
   (void)as;
+  //////////////////////END//////////////////////////
   return 0;
 }
 
 int as_define_stack(struct addrspace *as, vaddr_t *stackptr) {
+  //////////////////////START//////////////////////////
   assert(as->as_stackpbase != 0);
-
-  *stackptr = USERSTACK;
-  return 0;
-}
-
-int as_copy(struct addrspace *old, struct addrspace **ret) {
-  struct addrspace *new;
-
-  new = as_create();
-  if (new == NULL) {
-    return ENOMEM;
-  }
-
-  new->as_vbase1 = old->as_vbase1;
-  new->as_npages1 = old->as_npages1;
-  new->as_vbase2 = old->as_vbase2;
-  new->as_npages2 = old->as_npages2;
-
-  if (as_prepare_load(new)) {
-    as_destroy(new);
-    return ENOMEM;
-  }
-
-  assert(new->as_pbase1 != 0);
-  assert(new->as_pbase2 != 0);
-  assert(new->as_stackpbase != 0);
-
-  memmove((void *)PADDR_TO_KVADDR(new->as_pbase1),
-          (const void *)PADDR_TO_KVADDR(old->as_pbase1),
-          old->as_npages1 * PAGE_SIZE);
-
-  memmove((void *)PADDR_TO_KVADDR(new->as_pbase2),
-          (const void *)PADDR_TO_KVADDR(old->as_pbase2),
-          old->as_npages2 * PAGE_SIZE);
-
-  memmove((void *)PADDR_TO_KVADDR(new->as_stackpbase),
-          (const void *)PADDR_TO_KVADDR(old->as_stackpbase),
-          DUMBVM_STACKPAGES * PAGE_SIZE);
-
-  *ret = new;
+  //////////////////////END//////////////////////////
+  *stackptr = USERSTACK; /* Initial user-level stack pointer */
   return 0;
 }
