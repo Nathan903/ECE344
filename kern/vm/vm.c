b7af400f0cad1e2851ad7e821df37cbb78867353
nathan <noreply@nsa.gov>
2024-04-10 01:59:07 -0400

    coremap

--- kern/vm/vm.c
+++ kern/vm/vm.c
@@ -11,40 +11,39 @@
 /* under dumbvm, always have 48k of user stack */
 #define DUMBVM_STACKPAGES    12
 
-typedef struct{
-  char state;
-  pid_t pid;
-} coremap_entry;
-#define FREE_STATE 0
-#define FIXED_STATE 1
-#define DIRTY_STATE 2
-#define CLEAN_STATE 3
-coremap_entry coremap[71];
-
+#define nullptr '\0'
 extern u_int32_t firstfree;   /* first free virtual address; set by start.S */
 extern u_int32_t firstpaddr;  /* address of first free physical page */
 extern u_int32_t lastpaddr;   /* one past end of last free physical page */
-
+char vm_bootstrapped=0;
+#include "coremap.c"
 void vm_bootstrap(void) { 
-  kprintf("vm_bootstrap: firstaddr%u lastaddr%u firstfree%u freesize:%uK\n",firstpaddr, lastpaddr, firstfree, (lastpaddr-firstpaddr)/1024);
+  kprintf("[vm_bootstrap]: firstaddr%u lastaddr%u firstfree%u freesize:%uK\n",firstpaddr, lastpaddr, firstfree, (lastpaddr-firstpaddr)/1024);
+  vm_bootstrapped=1;
+
 }
 
 static paddr_t getppages(unsigned long npages) {
-  
-  int spl;
-  paddr_t addr;
-
+  int spl;int pagei;
   spl = splhigh();
-
-  addr = ram_stealmem(npages);
-
+  /////////////////////////////  
+  assert((vm_bootstrapped) || ((!vm_bootstrapped)&&((curthread==nullptr) || (curthread->pid==0))));
+  if(vm_bootstrapped){
+    lock_acquire(&cmlock);
+    pagei = get_pages(npages, (curthread==nullptr) ? 0 : curthread->pid);
+    lock_release(&cmlock);
+  } else{ pagei = get_pages(npages, (curthread==nullptr) ? 0 : curthread->pid);}
+  ///////////////////////////
+  //kprintf("[addr] %u %u %u %u\n", I_TO_ADDR(pagei), firstpaddr,(lastpaddr-firstpaddr)/1024,(lastpaddr-firstpaddr)%1024);
   splx(spl);
-  return addr;
+  assert(pagei>=0);
+  return I_TO_ADDR(pagei);
 }
 
-/* Allocate/free some kernel-space virtual pages */
 vaddr_t alloc_kpages(int npages) {
-  kprintf("[alloc_kpages] %d\n",npages);
+  if(npages!=1) rp("NPAGES!=1"); assert(npages==1);
+  
+  //kprintf("alloc: firstaddr%u lastaddr%u firstfree%u freesize:%uK\n",firstpaddr, lastpaddr, firstfree, (lastpaddr-firstpaddr)/1024);
   paddr_t pa;
   pa = getppages(npages);
   if (pa == 0) {
@@ -53,11 +52,23 @@ vaddr_t alloc_kpages(int npages) {
   return PADDR_TO_KVADDR(pa);
 }
 
-void free_kpages(vaddr_t addr) {
-  /* nothing */
 
-  (void)addr;
+
+void free_kpages(vaddr_t addr) {
+  //rp("e"); kprintf("%u %u %d\n",addr,KADDR_I(addr),free_page_size());
+  assert(COREMAP_SIZE>KADDR_I(addr) ); 
+  lock_acquire(&cmlock);
+  coremap[ KADDR_I(addr) ].state=FREE_STATE;
+  lock_release(&cmlock);
 }
+
+
+
+
+
+
+
+
 
 int vm_fault(int faulttype, vaddr_t faultaddress) {
   vaddr_t vbase1, vtop1, vbase2, vtop2, stackbase, stacktop;
